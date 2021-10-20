b177e5250fcedb4e5ebca8921226e06de48f8370
Yeider Adrian Mina caicedo <yeideradrian.noba@gmail.com>
2021-10-20 18:28:32 -0500

    fix mixins

--- app/design/frontend/Tribal/tribal/Magento_Theme/web/js/mixins/customer.js
+++ app/design/frontend/Tribal/tribal/Magento_Theme/web/js/mixins/customer.js
@@ -3,30 +3,51 @@ define(
         'jquery',
         'ko',
         'Magento_Ui/js/modal/modal'
-
     ], function ($, ko, modal) {
-
         var mixin = {
             initialize: function () {
-                this.hasCookies = ko.observable(false);
-                this.hasCookiModal();
+                this._super();
+                this.handleModal = ko.observable(false);
             },
-
             hasCookiModal: function () {
                 let temp = $.cookie("welcome_modal");
                 if (temp) {
-                    this.hasCookies(true);
+                    return true;
                 } else {
-                    this.hasCookies(false);
+                    return false;
                 }
             },
             openModal: function () {
+
+                if (!this.handleModal() ||  this.hasCookiModal()) {
+                    return
+                }
+                this.optionModal();
                 $("#welcome_default").modal('openModal');
                 var date = new Date();
                 var minutes = 2;
                 date.setTime(date.getTime() + (minutes * 60 * 1000));
-                $.cookie('newsletter_modal', date, { path: '/',expire: date });//Set the cookies
+                $.cookie('welcome_modal', date, {path: '/', expire: date});//Set the cookies
+                this.handleModal(true);
+            },
+            optionModal: function () {
+                var options = {
+                    type: 'popup',
+                    modalClass: 'welcome_default_modal',
+                    title: 'Welcome',
+                    responsive: true,
+                    innerScroll: false,
+                    buttons: [{
+                        text: $.mage.__('Close'),
+                        class: '',
+                        click: function () {
+                            this.closeModal();
+                        }
+                    }]
+                }
+                var popup = modal(options, "#welcome_default");
             }
+
         }
         return function (target) {
             return target.extend(mixin)
